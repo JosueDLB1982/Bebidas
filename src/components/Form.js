0a15d633a83a81e592cb4b4d49f3186137f58d79
Josue_Lopez <josuedlopez1982@gmail.com>
2022-02-01 20:55:53 -0300

    Change componente Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,9 +2,42 @@
 
 const Form = () => {
     return (
-        <h1>
-            Formulario Aquí
-        </h1>
+        <form className="col-12">
+            <fieldset className="text-center">
+                <legend>Busca bebidas por Categoría o Ingrediente</legend>
+            </fieldset>
+
+            <div className="row mt-4">
+                <div className="col-md-4">
+                    <input
+                        name="name"
+                        className="form-control"
+                        type="text"
+                        placeholder="Buscar por Ingrediente"
+                    />
+                </div>
+                <div className="col-md-4">
+                    <select
+                        className="form-control"
+                        name="category"
+                    >
+                        <option
+                            value=""
+                        >
+                            -- Selecciona Categoría --
+                        </option>
+                    </select>
+                </div>
+                
+                <div className="col-md-4">
+                    <input
+                        type="submit"
+                        className="btn btn-block btn-primary"
+                        value="Buscar Bebidas"
+                    />
+                </div>
+            </div>
+        </form>
     )
 }
 
