54480e435f35b4223005c56d3aedddc1f5d96b82
Josue_Lopez <josuedlopez1982@gmail.com>
2022-02-03 01:24:16 -0300

    Change ContextModal.js
    Añadidos state y effect para mostar receta especifica

--- src/context/ContextModal.js
+++ src/context/ContextModal.js
@@ -9,6 +9,20 @@ const ProviderModal = (props) => {
     // state del Provider
     const [idRecipe, saveIdRecipe] = useState(null)
 
+    const [recipe, saveRecipe] = useState({})
+
+    // Una vez que identificamos una receta queremos hacer otra consulta a la API, esta vez a otro endpoint
+    useEffect(() => {
+        const getRecipe = async () => {
+            if(!idRecipe) return
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idRecipe}`
+            const result = await axios.get(url)
+            saveRecipe(result.data.drinks[0])
+        }
+        getRecipe()
+        
+    }, [idRecipe])
+
     return (
         <ContextModal.Provider
             value={{
